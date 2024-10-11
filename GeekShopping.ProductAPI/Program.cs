74694b0de747b58426143fe9caa650d2da7be69a
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-11 12:24:07 -0300

    feat: migrations and database

---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,33 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar a string de conexão e adicionar o DbContext
+var connectionString = builder.Configuration.GetSection("MySQLConnection")["MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
