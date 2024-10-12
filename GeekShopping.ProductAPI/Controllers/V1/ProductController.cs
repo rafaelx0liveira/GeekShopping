1970ac719482acaf6a102d799e2b0470df38a462
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-11 23:28:48 -0300

    feat: versioning the api and implementing endpoint

---
+++ GeekShopping.ProductAPI/Controllers/V1/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Asp.Versioning;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new System.ArgumentNullException(nameof(productRepository)); // Verify if the productRepository is null and throw an exception
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+
+            return Ok(products);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,9 +1,12 @@
+using Asp.Versioning;
 using AutoMapper;
 using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Repository;
 using GeekShopping.ProductAPI.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,13 +34,35 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add repositories and implementations to the DI container
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-// Add services to the container.
-builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add API versioning to the DI container
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version")
+    );
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+// Add versioned API explorer to generate correct Swagger documentation
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+// Swagger configuration to support API versioning
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping V1", Version = "v1.0" });
+    options.SwaggerDoc("v2", new OpenApiInfo { Title = "GeekShopping V2", Version = "v2.0" });
+});
+
+// Add controllers
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -45,7 +70,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        // Swagger endpoints for each version
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping V1");
+    });
 }
 
 app.UseAuthorization();
