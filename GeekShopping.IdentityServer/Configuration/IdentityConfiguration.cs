821e236373bfcfd9b6bed4125ead5dfe9e32e1ab
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-12 18:14:03 -0300

    feat: identity server configuration

---
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes => 
+            new List<ApiScope>
+            {
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data.")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("geek_shopping_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = { new Secret("geek_shopping_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "http://localhost:26922/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:26922/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
+    }
+}
