8f18f1a864f0a52448e7b1a588969e92953510a6
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-12 15:50:42 -0300

    feat: adding DbContext to IdentityServer

---
+++ GeekShopping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.IdentityServer.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+    }
+}
