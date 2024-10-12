5ac2c0448c56ccda61964e17fbf69f5c52d22806
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-12 12:41:06 -0300

    feat: productService and DI container injection

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,26 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Get the base address for the ProductAPI from the configuration.
+var baseAddress = builder.Configuration.GetSection("ServiceUrls")["ProductAPI"];
+
+// Validate the base address.
+if (string.IsNullOrEmpty(baseAddress))
+{
+    throw new InvalidOperationException("The base address for the ProductAPI is not configured.");
+}
+
+// Add the ProductService to the DI container.
+builder.Services.AddHttpClient<IProductService, ProductService>(client =>
+{
+    // Set the base address of the ProductAPI.
+    client.BaseAddress = new Uri(baseAddress);
+});
 
 var app = builder.Build();
 
---
+++ GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.Interfaces;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string _basePath = "api/v1/product";
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _client.GetAsync(_basePath);
+
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _client.GetAsync($"{_basePath}/{id}");
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel product)
+        {
+            var response = await _client.PostAsJson(_basePath, product);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error: {response.ReasonPhrase}");
+            }
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> UpdateProduct(long id, ProductModel product)
+        {
+            var response = await _client.PutAsJson(_basePath, product);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error: {response.ReasonPhrase}");
+            }
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _client.DeleteAsync($"{_basePath}/{id}");
+
+            if (!response.IsSuccessStatusCode) {
+                throw new HttpRequestException($"Error: {response.ReasonPhrase}");
+            }
+
+            return await response.ReadContentAs<bool>();
+        }
+    }
+}
