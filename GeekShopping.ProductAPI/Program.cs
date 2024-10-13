6cec6b40d6f8b17f559dd9e399792f8748f1641a
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-13 14:14:05 -0300

    feat: add pop-up for jwt token

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -60,6 +60,37 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping V1", Version = "v1.0" });
     options.SwaggerDoc("v2", new OpenApiInfo { Title = "GeekShopping V2", Version = "v2.0" });
+
+    options.EnableAnnotations(); // Enable annotations in Swagger documentation
+
+    // Add JWT Bearer authentication (Pop-up for JWT token)
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. Example: \"
+        + "Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
 });
 
 // Add controllers
