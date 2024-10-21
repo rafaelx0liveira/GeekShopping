35506780459df50e4a10604b3b23a5389560c012
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2024-10-21 15:27:59 -0300

    feat: cart icon on menu

---
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using GeekShopping.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(
+            IProductService productService,
+            ICartService cartService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+            _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
